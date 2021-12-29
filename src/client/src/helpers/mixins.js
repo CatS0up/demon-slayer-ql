11409e3508f9179b2da4c9f3bdc49de0404b7661
Damian <dwoz137@gmail.com>
2021-12-29 19:28:09 +0100

    New vue mixins
    Section and pagination

--- src/client/src/helpers/mixins.js
+++ src/client/src/helpers/mixins.js
@@ -11,3 +11,57 @@ export const toggle = {
     },
   },
 }
+
+export const pagination = {
+  data() {
+    return {
+      currentPage: 1,
+      perPage: 10,
+      lastPage: 0
+    }
+  },
+  methods: {
+    skipPage(direction) {
+      if(this.changePageValidaiton(direction))
+        this.currentPage += this.currentPage < this.lastPage ? direction : 0;
+      else
+        throw new Error('Direction can be equals only 1 or -1!');
+    },
+    setLastPage(index) {
+      this.lastPage = Math.abs(index);
+    },
+    changePageValidaiton(direction) {
+      return [1, -1].includes(direction);
+    }
+  }
+}
+
+export const customInput = {
+  props: {
+    value: String
+  },
+  methods: {
+     handle() {
+      this.$emit("input", this.value);
+    },
+  }
+}
+
+import { SECTION_TYPE, SUB_SECTION_TYPE } from "@/helpers/constants.js";
+import DocumentationSection from "@/components/DocumentationSection.vue";
+import SectionParagraph from "@/primitives/SectionParagraph.vue";
+
+export const section = {
+  components: {
+    DocumentationSection,
+    SectionParagraph,
+  },
+  computed: {
+    sectionType() {
+      return SECTION_TYPE;
+    },
+    subSectionType() {
+      return SUB_SECTION_TYPE;
+    },
+  },
+}
