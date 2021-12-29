4633e3a7c9e0bc59dbe32bf2ee393f7ebf6bde4b
Damian <dwoz137@gmail.com>
2021-12-29 19:30:32 +0100

    Add lang prop to CodeContainer
    This component is more felxible now

--- src/client/src/components/CodeContainer.vue
+++ src/client/src/components/CodeContainer.vue
@@ -1,19 +1,24 @@
 <template>
-  <code-highlight language="json">
-   
-      <slot/>
-  
+  <code-highlight :language="lang">
+    <slot />
   </code-highlight>
 </template>
 <script>
 import CodeHighlight from "vue-code-highlight/src/CodeHighlight.vue";
-import "vue-code-highlight/themes/duotone-sea.css";
+import "vue-code-highlight/themes/prism-okaidia.css";
 import "vue-code-highlight/themes/window.css";
 import "prism-es6/components/prism-markup-templating";
 import "prism-es6/components/prism-json";
+import "prism-es6/components/prism-graphql";
 
 export default {
   name: "CodeContainer",
+  props: {
+    lang: {
+      type: String,
+      required: true,
+    },
+  },
   components: {
     CodeHighlight,
   },
