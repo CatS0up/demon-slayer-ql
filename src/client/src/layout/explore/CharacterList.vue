0cdb1c02cab49dfa4c02bf1b428e8081aee3787d
Damian <dwoz137@gmail.com>
2021-12-29 19:36:56 +0100

    Finish logic of characters list
    Infinite scroll and get data from the graphql server

--- src/client/src/layout/explore/CharacterList.vue
+++ src/client/src/layout/explore/CharacterList.vue
@@ -1,15 +1,99 @@
 <template>
-  <div class="character-list">
-    <character-card v-for="n in 20" :key="n" />
+  <div class="character-list" v-if="hasLoadedCharacters">
+    <character-card
+      v-for="character in characters"
+      :key="character._id"
+      :character="character"
+    />
   </div>
+  <pulse-loader v-else />
 </template>
 
 <script>
+import { pagination } from "@/helpers/mixins.js";
+
+import CHARACTERS_QUERY from "@/graphql/Characters.gql";
 import CharacterCard from "@/components/CharacterCard.vue";
+import PulseLoader from "@/primitives/PulseLoader.vue";
 
 export default {
   name: "CharacterList",
-  components: { CharacterCard },
+  mixins: [pagination],
+  components: {
+    CharacterCard,
+    PulseLoader,
+  },
+  computed: {
+    hasLoadedCharacters() {
+      return !!this.characters.length;
+    },
+    variables() {
+      return {
+        perPage: this.perPage,
+        page: this.currentPage,
+      };
+    },
+  },
+  data() {
+    return {
+      characters: [],
+      isLoadingNext: false,
+    };
+  },
+  apollo: {
+    characters: {
+      query: CHARACTERS_QUERY,
+      skip: true,
+      debounce: 250,
+      prefetch: true,
+      update({ characters }) {
+        const { data, paginatorInfo } = characters;
+
+        this.setLastPage(paginatorInfo.lastPage);
+
+        return data;
+      },
+    },
+  },
+  methods: {
+    async showMore() {
+      this.skipPage(1);
+
+      this.$apollo.queries.characters.fetchMore({
+        variables: this.variables,
+        updateQuery({ characters }, { fetchMoreResult }) {
+          return {
+            characters: {
+              __typename: characters.__typename,
+              data: [...characters.data, ...fetchMoreResult.characters.data],
+              paginatorInfo: fetchMoreResult.characters.paginatorInfo,
+            },
+          };
+        },
+      });
+    },
+    handleScroll() {
+      if (
+        document.documentElement.scrollTop + window.innerHeight ===
+        document.documentElement.offsetHeight
+      )
+        this.showMore();
+    },
+  },
+  created() {
+    this.$apollo.queries.characters.setVariables({ ...this.variables });
+    this.$apollo.queries.characters.skip = false;
+  },
+
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+
+    this.characters.length = 10;
+  },
 };
 </script>
 
@@ -17,11 +101,8 @@ export default {
 .character-list {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(auto, 325px));
-  grid-auto-rows: 500px;
   justify-content: center;
   margin: 1rem;
   gap: 2rem;
-
-  min-height: auto;
 }
 </style>
