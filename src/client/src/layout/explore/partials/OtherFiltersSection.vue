96c8345f8f553f9aae3ecbc6b8b42fbbb1cf1c22
Damian <dwoz137@gmail.com>
2021-12-29 19:35:23 +0100

    OtherFiltersSection

---
+++ src/client/src/layout/explore/partials/OtherFiltersSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <aside-section title="Others">
+    <aside-list>
+      <aside-list-item>
+        <base-checkbox> Other characters </base-checkbox>
+      </aside-list-item>
+    </aside-list>
+  </aside-section>
+</template>
+
+<script>
+import AsideSection from "@/primitives/AsideSection.vue";
+import AsideList from "@/primitives/AsideList.vue";
+import AsideListItem from "@/primitives/AsideListItem.vue";
+import BaseCheckbox from "@/primitives/BaseCheckbox.vue";
+
+export default {
+  name: "OtherFiltersSection",
+  components: {
+    AsideSection,
+    AsideList,
+    AsideListItem,
+    BaseCheckbox,
+  },
+  data() {
+    return {
+      filters: [],
+    };
+  },
+};
+</script>
