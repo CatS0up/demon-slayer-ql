39c34f129f0a711c903458559e5e65300d1f6e52
Damian <dwoz137@gmail.com>
2022-01-09 22:07:39 +0100

    Avatar backend route

--- src/backend/routes/web.php
+++ src/backend/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\Character\Character;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('media/{character}/{image}', function (Character $character, string $image): Response {
+    $slug = Str::slug($character->name);
+
+    $avatarPath = "avatars/{$slug}/{$image}";
+
+    if (Storage::disk('public')->exists($avatarPath)) {
+
+        $avatarBase64 = Storage::disk('public')->get("/avatars/{$slug}/{$image}");
+
+        return response($avatarBase64)->header('Content-type', 'image/png');
+    }
+
+    return response('Picture not found', 404);
+})
+    ->name('character.avatar');
